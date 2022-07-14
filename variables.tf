1baafac08a6ffd859b40639d698c1c3fc6390b99
filipvagner <vagnerfilip@gmail.com>
2022-07-14 23:06:47 +0200

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "rg_name" {
+  description = "Resource group name"
+  type        = string
+  default     = ""
+}
+
+variable "rg_location" {
+  description = "Resource group location"
+  type        = string
+  default     = ""
+}
